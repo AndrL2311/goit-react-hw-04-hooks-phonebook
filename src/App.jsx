33ea721fd5002d91ac9270770f44dbfbd520fa34
Andrii Lypovetskyi <li_andr@ukr.net>
2021-10-19 21:26:45 +0300

    Update App.jsx
    LocalStorage hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import s from './App.module.css';
@@ -7,30 +7,25 @@ import Filter from './components/Filter/Filter';
 import Contacts from './components/Contacts/Contacts';
 
 function App() {
-  const data = [
+  const defaultContacts = [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ];
 
-  const [contacts, setContacts] = useState(data);
+  const useLocalStoage = (key, defaultValue) => {
+    const [state, setState] = useState(() => {
+      return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
+    });
+    useEffect(() => {
+      window.localStorage.setItem(key, JSON.stringify(state));
+    }, [key, state]);
+    return [state, setState];
+  };
+
+  const [contacts, setContacts] = useLocalStoage('contacts', defaultContacts);
   const [filter, setFilter] = useState('');
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (this.state.contacts !== prevState.contacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  //   }
-  // }
-
-  // componentDidMount() {
-  //   const contacts = localStorage.getItem('contacts');
-  //   const parsedContacts = JSON.parse(contacts);
-
-  //   if (parsedContacts) {
-  //     this.setState({ contacts: parsedContacts });
-  //   }
-  // }
 
   const formSubmitHandler = data => {
     // console.log(data);
@@ -52,14 +47,22 @@ function App() {
     setFilter(event.currentTarget.value);
   };
 
-  const getVisibleContacts = () => {
+  const visibleContacts = useMemo(() => {
     const normalizedFilter = filter.toLowerCase();
+    console.log(contacts);
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter),
     );
-  };
+  }, [contacts, filter]);
 
-  const visibleContacts = getVisibleContacts();
+  // const getVisibleContacts = () => {
+  //   const normalizedFilter = filter.toLowerCase();
+  //   return contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(normalizedFilter),
+  //   );
+  // };
+
+  // const visibleContacts = getVisibleContacts();
 
   return (
     <div className={s.container}>
